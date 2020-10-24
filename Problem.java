53661ec8a96540024b88b5b3f4543887bc9d68ca
erikayao93 <71943596+erikayao93@users.noreply.github.com>
2020-10-24 04:23:05 -0500

    Add files via upload

---
+++ Problem.java
@@ -0,0 +1,16 @@
+
+public class Problem {
+	private String question;
+	private String answer;
+	public Problem(String q, String a) {
+		question = q;
+		answer = a;
+	}
+	
+	public String getQ() {
+		return question;
+	}
+	public String getA() {
+		return answer;
+	}
+}
