6f8c645e9cee09cb56951d61e88a738b4e2b6ba0
tahaismail424 <taha.ismail424@gmail.com>
2020-10-24 23:54:38 -0500

    added bounds

--- TheGoonFighters.js
+++ TheGoonFighters.js
@@ -1,27 +1,38 @@
-window.onload=displayOpeningMessage()
 
-displayOpeningMessage{
-    alert("You awaken in a grassy field, ringed by a forest. In the far, far distance, you see mountains. \
-    You aren’t sure where you are, or how you got there, but you know you need to get out of the wilderness.")
-}
-var countStepVert=0;
-var countStepHor=0;
+window.onload = alert("You awaken in a grassy field, ringed by a forest. In the far, far distance, you see mountains. \
+You aren’t sure where you are, or how you got there, but you know you need to get out of the wilderness.");
+
+let countStepVert = 0;
+
+let countStepHor = 0;
 function eastButtonClick() {
     alert ("Move East");
     countStepHor++;
+    checkBounds();
 }
 function westButtonClick() {
-    alert ("Move West")
+    alert ("Move West");
     countStepHor--;
+    checkBounds();
 }
-function northButtonClick(){
-    alert ("Move North")
+function northButtonClick() {
+    alert ("Move North");
     countStepVert++;
-}
+    checkBounds();
+   
+    }
 function southButtonClick(){
-    alert ("Move South")
+    alert ("Move South");
     countStepVert--;
+    checkBounds();
 }
-if (Math.abs(countStepVert)>2){
-    alert()
+
+
+function checkBounds() {
+
+    if (Math.abs(countStepVert) > 2 || Math.abs(countStepHor) > 2) {
+        alert("You went out of bounds!");
+        countStepVert = 0;
+        countStepHor = 0;
+    }
 }
