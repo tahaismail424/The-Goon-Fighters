3bbed7d5ec6e253de2b0283709876151482fecf2
Erika Yao <erikayyao@gmail.com>
2020-10-25 04:00:20 -0500

    files with quit button added Please enter the commit message for your changes. Lines starting

--- TheGoonFighters.js
+++ TheGoonFighters.js
@@ -1,4 +1,5 @@
 
+
 window.onload = alert("You awaken in a grassy field, ringed by a forest. In the far, far distance, you see mountains. \
 You are not sure where you are, or how you got there, but you know you need to get out of the wilderness.");
 
@@ -21,16 +22,18 @@ function northButtonClick() {
     countStepVert++;
     checkBounds();
    
-    }
+}
 function southButtonClick(){
     alert ("Move South");
     countStepVert--;
     checkBounds();
 }
 
+function quit(){
+    window.close();
+}
 
 function checkBounds() {
-
     if ((Math.abs(countStepVert) > 2 || Math.abs(countStepHor) > 2) && scene == 0) {
         alert("You emerge from the field, into a forest.");
         scene++;
@@ -51,34 +54,6 @@ function checkBounds() {
         alert("You cross back over the bridge, into the forest.");
         scene--;
         my_function(scene);
-
     }
 }
-/*
-function changeToForest(){
-   prettyStuff.getElementById('page_body').className = "forest.png";
-}
 
-
-var background = $('body');
-
-       var backgrounds = new Array(
-        'url(img/fields.jpg)',
-        'url(img/forest.png)',
-        'url(img/mountain.jpg)'
-    );
-
-    var current = 0;
-    function nextBackground(){
-        current++;
-        current = current % backgrounds.length;
-        background.css('background-image', backgrounds[current]);
-    }
-
-    background.css('background-image', backgrounds[0]);
-
-$(function() {
-    
-
-});
-*/
